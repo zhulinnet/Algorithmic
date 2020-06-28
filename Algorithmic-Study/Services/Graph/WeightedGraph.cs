0dfeeae37a33186847e635aa254a4807dbcbef62
zhulinnet <princess93@qq.com>
2020-06-28 16:23:07 +0800

    add Bellman-Ford

---
+++ Algorithmic-Study/Services/Graph/WeightedGraph.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace algorithmStudy.Services.Graph
+{
+    public class WeightedGraphNode<T>
+    {
+        public T Data { get; set; }
+        //初始权重：起点为0，其他顶点为无穷大（∞）
+        public double Weight { get; set; } = double.MaxValue;
+        public Dictionary<T, double> Children { get; set; }
+    }
+    public class WeightedGraph<T> where T : IComparable
+    {
+        public List<WeightedGraphNode<T>> NodeList { get; set; }
+        private bool IsWeightChange { get; set; } = false;
+        private List<T> ShortestPath { get; set; } = new List<T>();
+        private int updateNum { get; set; } = 0;
+        public WeightedGraph()
+        {
+            this.NodeList = null;
+        }
+        public WeightedGraph(List<WeightedGraphNode<T>> graphList)
+        {
+            this.NodeList = graphList;
+        }
+        public void BellmanFord()
+        {
+            if (NodeList == null)
+            {
+                //图为空
+                return;
+            }
+            updateNum++;
+            if (updateNum >= NodeList.Count)
+            {
+                //该图有负权回路
+                return;
+            }
+            foreach (var node in NodeList)
+            {
+                if (!IsWeightChange)
+                {
+                    return;
+                }
+                Dictionary<T, double> children = node.Children;
+                if (children != null)
+                {
+                    foreach (var itemC in children)
+                    {
+                        WeightedGraphNode<T> nodeChildren = NodeList.FirstOrDefault(s => s.Data.CompareTo(itemC.Key) == 0);
+                        var currentWeight = node.Weight + itemC.Value;
+                        if (currentWeight.CompareTo(nodeChildren.Weight) < 0)
+                        {
+                            nodeChildren.Weight = currentWeight;
+                            IsWeightChange = true;
+                        }
+                        else
+                        {
+                            IsWeightChange = false;
+                        }
+                    }
+                }
+            }
+            BellmanFord();
+        }
+
+        public List<T> GetShortest(T start, T end)
+        {
+            if (end == null)
+            {
+                ShortestPath.Reverse();
+                return ShortestPath;
+            }
+            WeightedGraphNode<T> nodeEnd = NodeList.FirstOrDefault(s => s.Data.CompareTo(end) == 0);
+            Dictionary<T, double> children = nodeEnd.Children;
+            ShortestPath.Add(end);
+            if (children != null)
+            {
+                var minData = default(T);
+                foreach (var itemC in children)
+                {
+                    WeightedGraphNode<T> nodeC = NodeList.FirstOrDefault(s => s.Data.CompareTo(itemC.Key) == 0);
+
+                    if ((nodeEnd.Weight - itemC.Value).CompareTo(nodeC.Weight) == 0)
+                    {
+                        minData = nodeC.Data;
+                        break;
+                    }
+                }
+                return GetShortest(start, minData);
+            }
+            ShortestPath.Reverse();
+            return ShortestPath;
+        }
+    }
+}
---
+++ algorithmStudyTests/Services/Graph/WeightedGraphTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using algorithmStudy.Services.Graph;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace algorithmStudy.Services.Graph.Tests
+{
+    [TestClass()]
+    public class WeightedGraphTests
+    {
+        [TestMethod()]
+        public void BellmanFordTest()
+        {
+            WeightedGraph<string> weightedGraph = new WeightedGraph<string>();
+            List<WeightedGraphNode<string>> seed = new List<WeightedGraphNode<string>>
+            {
+                new WeightedGraphNode<string>() { Data = "A",Weight=0, Children = new Dictionary<string, double>{  {"B",9},{"C",2} }  },
+                new WeightedGraphNode<string>() { Data = "B", Children = new Dictionary<string, double>{ { "A", 9 }, { "C", 6 }, { "D",3},{"E",1} }  },
+                new WeightedGraphNode<string>() { Data = "C", Children = new Dictionary<string, double>{ { "A", 2 }, { "B",6},{"D",2}, { "F", 9 } }  },
+                new WeightedGraphNode<string>() { Data = "D", Children = new Dictionary<string, double>{  {"B",3},{"C",2}, { "E", 5 } , { "F", 6 } }  },
+                new WeightedGraphNode<string>() { Data = "E", Children = new Dictionary<string, double>{  {"B",1},{"D",5}, { "F", 3 }, { "G", 7 } }  },
+                new WeightedGraphNode<string>() { Data = "F", Children = new Dictionary<string, double>{  {"C",9},{"D",6}, { "E", 3 } , { "G", 4 } } },
+                new WeightedGraphNode<string>() { Data = "G", Children =new Dictionary<string, double>{  {"E",7},{"F",4} } },
+            };
+            weightedGraph.NodeList = seed;
+            weightedGraph.BellmanFord();
+            List<string> result = new List<string>() {
+                "A","C","D","F","G"
+            };
+            CollectionAssert.AreEqual(weightedGraph.GetShortest("A", "G"), result);
+        }
+    }
+}
