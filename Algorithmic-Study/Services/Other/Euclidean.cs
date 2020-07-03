3a58b5de068b8be4bd3dfd352bb1467d3cf599fd
zhulinnet <princess93@qq.com>
2020-07-03 09:49:01 +0800

    add Euclidean

---
+++ Algorithmic-Study/Services/Other/Euclidean.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace algorithmStudy.Services.Other
+{
+    /// <summary>
+    /// 欧几里得算法
+    /// </summary>
+    public class Euclidean
+    {
+        /// <summary>
+        /// 求最大公约数
+        /// </summary>
+        /// <param name="numberA"></param>
+        /// <param name="numberB"></param>
+        /// <returns></returns>
+        public int GetGCD(int numberA, int numberB)
+        {
+            var gcd = 0;
+            var remainder = 0;
+            if (numberA > numberB)
+            {
+                remainder = numberA % numberB;
+                gcd = numberB;
+            }
+            else
+            {
+                remainder = numberB % numberA;
+                gcd = numberA;
+            }
+            if (remainder == 0)
+            {
+                return gcd;
+            }
+            return GetGCD(remainder, gcd);
+        }
+    }
+}
---
+++ algorithmStudyTests/Services/Other/EuclideanTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using algorithmStudy.Services.Other;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace algorithmStudy.Services.Other.Tests
+{
+    [TestClass()]
+    public class EuclideanTests
+    {
+        [TestMethod()]
+        public void GetGCDTest()
+        {
+            Euclidean euc = new Euclidean();
+            Assert.AreEqual(euc.GetGCD(1112, 695),139);
+        }
+    }
+}
